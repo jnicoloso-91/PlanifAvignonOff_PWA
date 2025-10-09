a2e3d9c7a4e511036a79eeeb9d6006803e9cc8b1
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-10-09 17:02:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -805,6 +805,7 @@ document.addEventListener('DOMContentLoaded', () => {
   lockHorizontalScroll();
   initSafeAreaWatch();
   initBottomBarAutoLayout();   
-  setTimeout(syncBottomBarTogglePosition, 300);
-  setTimeout(syncSafeLayout, 300); // re-sync after 1s (iOS late adjustments )
+  setTimeout(syncBottomBarTogglePosition, 1000);
+  setTimeout(syncSafeLayout, 1000); // re-sync after 1s (iOS late adjustments )
 });
+
