f8dce3b923689592747892f4975044191a9bedc1
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-10-26 10:09:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2146,8 +2146,8 @@ function gridOptionsCommon(gridId, el) {
     defaultColDef: { editable: true, resizable: true, sortable: true, filter: true },
     rowData: [],
     getRowId: p => p.data?.__uuid,
-popupParent: document.body,
-suppressRowTransforme: true, // IPad
+// popupParent: document.body,
+// suppressRowTransform: true, // IPad
     onGridReady: async (p) => {
       await refreshGrid(gridId);
       safeSizeToFitFor(gridId);
