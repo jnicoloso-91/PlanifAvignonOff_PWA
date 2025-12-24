fcd8b22b73457292f5bba42613007040c93e19ae
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-12-24 10:11:50 +0100

    Add files via upload

--- pager.js
+++ pager.js
@@ -21,19 +21,13 @@ import {
 	// const hasDF = window.ctx?.df && window.ctx.df.length > 0;
 	// let index = hasDF ? 0 : 1; // 1 = planning, 0 = catalogues
 
-  let dragging = false, engaged = false;
+	let dragging = false, engaged = false;
   let startX = 0, startY = 0, curX = 0;
-  let pageW = computePageW() ; //pager.clientWidth || window.innerWidth || 1;
+  let pageW = pager.clientWidth || window.innerWidth || 1;
 
   function measure(){
-    pageW = computePageW() ; //pager.clientWidth || window.innerWidth || 1;
+    pageW = pager.clientWidth || window.innerWidth || 1;
   }
-
-  function computePageW() {
-    const rect = pager.getBoundingClientRect();
-    const dpr = window.devicePixelRatio || 1;
-    return Math.round(rect.width * dpr) / dpr;
-	}
 
   function applyTransform(px, animate=false){
     track.style.transition = animate ? 'transform .25s ease' : 'none';
