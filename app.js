023291643990589b7af602d1118b0c8ce6cc3473
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-12-24 09:14:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1927,7 +1927,7 @@ function buildColumnsActivitesCommon(){
       }
     },
     { field:'Activite', headerName: 'Activité', minWidth:200, flex:1.5, cellRenderer: ActiviteRenderer },
-    { field:'__desc_summary', headerName: '', width, minWidth:40, filter: false, sortable: false ,  cellRenderer: infosPlusPopoverCellRenderer },
+    // { field:'__desc_summary', headerName: '', width, minWidth:40, filter: false, sortable: false ,  cellRenderer: infosPlusPopoverCellRenderer },
     { field:'Style', headerName: 'Style', minWidth:150, flex:0.6 },
     { field:'Mood', headerName: 'Ton', minWidth:150, flex:0.6 },
     { field:'Note', headerName: 'Note', width, minWidth:width },
