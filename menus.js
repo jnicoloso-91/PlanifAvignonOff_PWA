33053ae3b0065020dc5ede710f2de09c6862f5fc
jnicoloso-91 <joel.nicoloso@gmail.com>
2026-04-09 12:03:46 +0200

    Add files via upload

--- menus.js
+++ menus.js
@@ -1127,7 +1127,7 @@ export function wireAppKebab() {
           ]
         },
         { id:'help',      label:'Aide',                     onClick: ()=>openSheetAide() },
-        { id:'JsonOff',   label:'Export JSON Off',          onClick: async ()=> await exportJsonForAi('off', 2026) },
+        // { id:'JsonOff',   label:'Export JSON Off',          onClick: async ()=> await exportJsonForAi('off', 2026) },
         // { id:'JsonIn',    label:'Export JSON In',           onClick: async ()=> await exportJsonForAi('in', 2026) },
       ]
     });
