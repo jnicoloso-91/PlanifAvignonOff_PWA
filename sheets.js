7e9f59dfed54201d8ec5ea3914cb37f27a643518
jnicoloso-91 <joel.nicoloso@gmail.com>
2026-05-13 02:46:11 +0200

    Add files via upload

--- sheets.js
+++ sheets.js
@@ -7532,7 +7532,7 @@ export function openSheetSearch({ title = "Chercher", initialQuery = "" } = {}){
       if (!expEl) return;
 
       expEl.scrollIntoView({
-        behavior: "auto",   // ou "auto" instantané
+        behavior: "smooth",   // ou "auto" instantané
         block: "nearest",     // "start" | "center" | "end" | "nearest"
       });
     }
